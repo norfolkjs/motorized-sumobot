d4dc2dbe9e63f24479eea9e192614c297561c847
Linda Nichols <lynnaloo@gmail.com>
2016-07-23 17:36:37 -0400

    Create j-5 hello world

---
+++ eg/hello_world.js
@@ -0,0 +1,9 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+  led.blink(500);
+  
+  console.log('Did you see a blink?');
+});
